2b6d38df014664db751792fc556e59295f0313fe
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-04 17:04:05 -0400

    tela inicial do app

---
+++ config.md
@@ -0,0 +1,19 @@
+#Configuração base
+
+```
+nvm install 16
+nvm use 16
+``` 
+
+```
+npm install -g expo-cli
+```
+
+Configurar o login do expo e entra no tunelamento
+```
+expo login
+
+expo install expo
+
+npx expo start --tunnel
+```
---
+++ src/components/MoviesCard/index.js
@@ -0,0 +1,15 @@
+import { TouchableOpacity, Image, StyleSheet } from "react-native";
+
+export const MovieCard = (props) => { 
+    return (
+        <TouchableOpacity>
+            <Image style={styles.img} source={props.movieURL} />
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({ 
+    img: {
+        marginRight: 20 ,
+    },
+});
